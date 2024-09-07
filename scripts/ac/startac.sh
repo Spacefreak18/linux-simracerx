8f3759dd2f7a6ac0fa916190eed1e291df03d2b5
Paul Dino Jones <paul@spacefreak18.xyz>
2024-09-07 13:21:56 -0400

    Use send-keys to keep panes alive when program quits so restarting the command is easier

--- scripts/ac/startac.sh
+++ scripts/ac/startac.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-tmux new-session -d -s "RacerX"
+tmux new-session -d -s "racerx"
 
-tmux new-window -t RacerX:1 -n createsimshm '~/shmwrap/ac/createsimshm;'
-tmux new-window -t RacerX:2 -n monocoque 'monocoque play;'
-tmux new-window -t RacerX:3 -n simmonitor 'FBROTATE=1 FBDEV=/dev/fb1 simmonitor play -u fb;'
-tmux new-window -t RacerX:4 -n simmonitor-telemetry 'simmonitor play -u cli -m;'
-tmux new-window -t RacerX:5 -n crewchief '~/.local/bin/startcrewac.sh; zsh -i;'
+tmux new-window -t racerx:1 -n createsimshm
+tmux send-keys -t racerx:1 "~/shmwrap/ac/createsimshm" Enter
+
+tmux new-window -t racerx:2 -n monocoque
+tmux send-keys -t racerx:2 "sleep 3 && monocoque play" Enter
+
+tmux new-window -t racerx:3 -n simmonitorvocore
+tmux send-keys -t racerx:3 "sleep 3 && FBROTATE=1 FBDEV=/dev/fb1 simmonitor play -u fb" Enter
+
+tmux new-window -t racerx:4 -n simmonitortelem
+tmux send-keys -t racerx:4 "sleep 3 && simmonitor play -u cli -m" Enter
+
+tmux new-window -t racerx:5 -n simmonitorflags
+tmux send-keys -t racerx:5 "sleep 3 && simmonitor play -u web --uiconf /home/racerx/.config/simmonitor/simmonitor.config.flags" Enter
+
+tmux new-window -t racerx:6 -n devtodev
+tmux send-keys -t racerx:6 "sleep 3 && devtodev /dev/fb1 /dev/fb0" Enter
+
+tmux new-window -t racerx:7 -n open
+tmux send-keys -t racerx:7 "sleep 3" Enter
+
+tmux new-window -t racerx:8 -n crewchief
+tmux send-keys -t racerx:8 "sleep 3 && ~/.local/bin/startcrewac.sh" Enter
